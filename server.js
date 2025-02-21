3168a23f71659a35990f7fefc9b237e2905e1c14
stratenchuk <ystratenchuk@mail.ru>
2025-02-21 20:42:10 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+//
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hellow!</h1>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
+});
+
+//
